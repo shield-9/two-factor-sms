4eeb9bb819f77a43b2aff2c9d83d01c3e84706f1
Daisuke Takahashi <daisuke@extendwings.com>
2015-08-28 01:21:57 +0900

    Compatiblility with older PHP
    WP.org Repo is using old PHP, lol.

--- class.two-factor-sms.php
+++ class.two-factor-sms.php
@@ -223,10 +223,10 @@ class Two_Factor_Sms extends Two_Factor_Provider {
 	 */
 	public function is_available_for_user( $user ) {
 		return (
-			! empty( get_user_meta( $user->ID, self::ACCOUNT_SID_META_KEY, true ) )
-			 && ! empty( get_user_meta( $user->ID, self::AUTH_TOKEN_META_KEY, true ) )
-			 && ! empty( get_user_meta( $user->ID, self::SENDER_NUMBER_META_KEY, true ) )
-			 && ! empty( get_user_meta( $user->ID, self::RECEIVER_NUMBER_META_KEY, true ) )
+			get_user_meta( $user->ID, self::ACCOUNT_SID_META_KEY, true ) !== false
+			 && get_user_meta( $user->ID, self::AUTH_TOKEN_META_KEY, true ) !== false
+			 && get_user_meta( $user->ID, self::SENDER_NUMBER_META_KEY, true ) !== false
+			 && get_user_meta( $user->ID, self::RECEIVER_NUMBER_META_KEY, true ) !== false
 		);
 	}
 
